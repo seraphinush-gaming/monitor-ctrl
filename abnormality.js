cd86b1c4a7c5a09aab2a45a46eb61d535f9d34b9
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-03-20 23:38:52 +0900

    1.23 auto-update support

---
+++ abnormality.js
@@ -0,0 +1,9 @@
+module.exports = [
+    70234, // 
+    70235, // 
+    70236, // 하늘 연꽃주
+    70237, // Lein's Rootbeer
+    70238, // 터커식 꿀호박술
+    905434,
+    999001011, // Beach Party : Watermelon mini-game
+]
---
+++ crystal.js
@@ -0,0 +1,8 @@
+module.exports = [
+    10306, // Forcefulness VII
+    10316, // Forcefulness VII
+    12001, // Infusion VII
+    12003, // Infusion VII
+    12120, // Infusion VII
+    12130, // Infusion VII
+]
