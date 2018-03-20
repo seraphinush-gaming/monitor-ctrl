0d9e5613d24f41111af614c5b4e055260efc2b69
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-03-20 23:37:12 +0900

    1.23 r:00

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-// OPCODE REQUIRED :
-// - S_ABNORMALITY_BEGIN
-// - S_ABNORMALITY_REFRESH
-// - S_START_ACTION_SCRIPT
+// Version 1.23 r:00
 
-// Version 1.22 r:01
-
-const abnormality = [
-    70234, // 
-    70235, // 
-    70236, // 하늘 연꽃주
-    70237, // Lein's Rootbeer
-    70238, // 터커식 꿀호박술
-    905434,
-    999001011, // Beach Party : Watermelon mini-game
-]
-const crystal = [
-    10306, // Forcefulness VII
-    10316, // Forcefulness VII
-    12001, // Infusion VII
-    12003, // Infusion VII
-    12120, // Infusion VII
-    12130, // Infusion VII
-]
+const abnormality = require('./abnormality.js')
+const crystal = require('./crystal.js')
 
 module.exports = function MonitorControl(d) {
 
