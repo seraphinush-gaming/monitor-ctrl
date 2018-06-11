054c4a15e3c705665bc9595cb88b4c642c52b127
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-06-11 17:12:23 +0900

    1.23 r:02

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Version 1.23 r:01
+// Version 1.23 r:02
 
 const abnormality = require('./abnormality.js')
 const crystal = require('./crystal.js')
@@ -6,10 +6,10 @@ const crystal = require('./crystal.js')
 module.exports = function MonitorControl(d) {
 
     // block drunken screen abnomality
-    d.hook('S_ABNORMALITY_BEGIN', (e) => { if (abnormality.includes(e.id)) return false })
+    d.hook('S_ABNORMALITY_BEGIN', 2, (e) => { if (abnormality.includes(e.id)) return false })
 
     // block crystal effect refresh
-    d.hook('S_ABNORMALITY_REFRESH', (e) => { if (crystal.includes(e.id)) return false })
+    d.hook('S_ABNORMALITY_REFRESH', 1, (e) => { if (crystal.includes(e.id)) return false })
 
     // block screen zoom scripts
     d.hook('S_START_ACTION_SCRIPT', 'raw', () => { return false })
