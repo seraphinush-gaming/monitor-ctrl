03988e9ef9edfff6fb7095ea7535c00e642f3e8c
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-09-04 20:41:44 -0700

    Delete crystal.js

--- crystal.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-    10306, // Forcefulness VII
-    10316, // Forcefulness VII
-    12001, // Infusion VII
-    12003, // Infusion VII
-    12120, // Infusion VII
-    12130, // Infusion VII
-]
