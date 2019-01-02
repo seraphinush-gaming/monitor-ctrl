ebf9f1f49d6b270c15ac0b072c6e92d0cc24d84d
seraph <seraphinush@gmail.com>
2019-01-01 20:28:01 -0800

    1.28.01

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const config = require('./config.js');
 
 module.exports = function MonitorCtrl(mod) {
 
+    let flag = false;
+
     // block drunken screen abnomality
     mod.hook('S_ABNORMALITY_BEGIN', 3, (e) => {
         if (config.abnormality.includes(e.id))
             return false;
+        else if (config.crystal.includes(e.id)) {
+            flag = true;
+            mod.setTimeout(() => { flag = false }, 4000);
+        }
     });
 
     // block crystal effect refresh
     mod.hook('S_ABNORMALITY_REFRESH', 1, (e) => {
         if (config.crystal.includes(e.id))
-            return false;
+            return flag;
     });
 
     // block screen zoom scripts
